63c0ce50963e05aef5a16cd0cb1f94383f862515
SnippyRex <kentf022@gmail.com>
2023-08-26 00:53:57 +0100

    Set up Compose Navigation

--- app/src/main/java/com/example/replica/MainActivity.kt
+++ app/src/main/java/com/example/replica/MainActivity.kt
@@ -6,10 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.replica.composables.YnabScreen
 import com.example.replica.ui.theme.ReplicaTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +24,23 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Replica()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+sealed class Screen(val route: String){
+    object Ynab : Screen("YNAB")
 }
 
-@Preview(showBackground = true)
+
 @Composable
-fun GreetingPreview() {
-    ReplicaTheme {
-        Greeting("Android")
+fun Replica( modifier: Modifier = Modifier) {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = Screen.Ynab.route ){
+        composable(Screen.Ynab.route){ YnabScreen()}
     }
 }
