02bbc72976a2f08c114a08d6687651ea4c083257
SnippyRex <kentf022@gmail.com>
2023-08-29 01:01:04 +0100

    Optimize Imports

--- app/src/main/java/com/example/replica/composables/YnabScreen.kt
+++ app/src/main/java/com/example/replica/composables/YnabScreen.kt
@@ -27,7 +27,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -36,10 +35,9 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.replica.R
 
-@OptIn(ExperimentalMaterial3Api::class)
+
 @Composable
 fun YnabScreen() {
-
     Column(modifier = Modifier.fillMaxSize(1f)) {
         ElevatedCenterAlignedTopAppBar()
         Divider(color = Color.LightGray)
@@ -158,6 +156,7 @@ fun NumberPad() {
 
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ElevatedCenterAlignedTopAppBar() {
     Surface(shadowElevation = 6.dp, modifier = Modifier.fillMaxWidth()) {
