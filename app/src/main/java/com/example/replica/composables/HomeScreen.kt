bf38ed5e268037d4dc8dd45d07cbc1bc60ba292a
SnippyRex <kentf022@gmail.com>
2023-08-29 14:41:37 +0100

    Add link to Github portfolio

--- app/src/main/java/com/example/replica/composables/HomeScreen.kt
+++ app/src/main/java/com/example/replica/composables/HomeScreen.kt
@@ -11,8 +11,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -20,6 +23,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -29,13 +33,17 @@ import com.example.replica.R
 
 @Composable
 fun HomeScreen(ynabScreen: () -> Unit) {
+    val uriHandler = LocalUriHandler.current
+    val portfolioLink = "https://github.com/SnippyRex"
 
     Column(modifier = Modifier.fillMaxSize()) {
         ElevatedBar(title = {Text(text = stringResource(id = R.string.app_name), fontSize = 40.sp, fontWeight = FontWeight.SemiBold, color = Color(135, 187, 70, 255))})
         Spacer(modifier = Modifier.size(16.dp))
         Text(text = "A showcase of App Screen Recreations", textAlign = TextAlign.Center,modifier = Modifier.fillMaxWidth())
         Spacer(modifier = Modifier.size(32.dp))
-        Box(modifier = Modifier.padding(horizontal = 60.dp).fillMaxWidth()) {
+        Box(modifier = Modifier
+            .padding(horizontal = 60.dp)
+            .fillMaxWidth()) {
             Row(modifier = Modifier.align(alignment = Alignment.Center)) {
                 TextButton(onClick = { ynabScreen() }, modifier = Modifier.background(
                     Color(135, 187, 70, 100))) {
@@ -43,9 +51,13 @@ fun HomeScreen(ynabScreen: () -> Unit) {
                 }
             }
         }
+        Row {
+            Icon(Icons.Filled.Share, contentDescription = "Link")
+            TextButton(onClick = { uriHandler.openUri(portfolioLink) }) {
+                Text(text = "Creator Portfolio")
+            }
+        }
     }
-
-    
 }
 
 
