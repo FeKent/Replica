8ea0e1161a9bf9283d7d613825a8ab405029c360
SnippyRex <kentf022@gmail.com>
2023-08-30 13:44:37 +0100

    Add WhatsApp's default background image to screen

--- app/src/main/java/com/example/replica/composables/WhatsAppScreen.kt
+++ app/src/main/java/com/example/replica/composables/WhatsAppScreen.kt
@@ -3,8 +3,10 @@
 package com.example.replica.composables
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
@@ -20,7 +22,9 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.replica.R
@@ -32,32 +36,40 @@ fun WhatsAppScreen(backHome: () -> Unit) {
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WhatsAppBar(backHome: () -> Unit) {
-    TopAppBar(title = {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Icon(Icons.Filled.Person, "Profile Picture", modifier = Modifier.size(30.dp))
-            Spacer(modifier = Modifier.size(8.dp))
-            Text(text = "Hercule Poirot", modifier = Modifier.fillMaxWidth(1f))
-
-        }
-    }, navigationIcon = {
-        IconButton(onClick = { backHome() }) {
-            Icon(Icons.Filled.ArrowBack, "Back", modifier = Modifier.size(20.dp))
-        }
-    }, actions = {
-        Icon(
-            painter = painterResource(id = R.drawable.video_call),
-            contentDescription = "Video Call"
+    Box(modifier = with(Modifier) {
+        fillMaxSize().paint(
+            painterResource(R.drawable.whatsapp_background),
+            contentScale = ContentScale.FillBounds
         )
-        Spacer(modifier = Modifier.size(16.dp))
-        Icon(Icons.Filled.Call, "Call Icon")
-        Spacer(modifier = Modifier.size(8.dp))
-        Icon(Icons.Filled.MoreVert, "More Options")
-    },
-        modifier = Modifier.background(color = Color.LightGray)
-    )
+    }) {
+        TopAppBar(title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Icon(Icons.Filled.Person, "Profile Picture", modifier = Modifier.size(30.dp))
+                Spacer(modifier = Modifier.size(8.dp))
+                Text(text = "Hercule Poirot", modifier = Modifier.fillMaxWidth(1f))
+
+            }
+        }, navigationIcon = {
+            IconButton(onClick = { backHome() }) {
+                Icon(Icons.Filled.ArrowBack, "Back", modifier = Modifier.size(20.dp))
+            }
+        }, actions = {
+            Icon(
+                painter = painterResource(id = R.drawable.video_call),
+                contentDescription = "Video Call"
+            )
+            Spacer(modifier = Modifier.size(16.dp))
+            Icon(Icons.Filled.Call, "Call Icon")
+            Spacer(modifier = Modifier.size(8.dp))
+            Icon(Icons.Filled.MoreVert, "More Options")
+        },
+            modifier = Modifier.background(color = Color.LightGray)
+        )
+    }
 }
