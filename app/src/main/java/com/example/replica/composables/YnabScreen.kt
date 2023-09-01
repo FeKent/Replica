4a29065595b9dc5872b6410377beac56b59d42c1
Stuart Kent <stuart@stkent.com>
2023-09-01 10:48:06 -0400

    Add YNAB preview

--- app/src/main/java/com/example/replica/composables/YnabScreen.kt
+++ app/src/main/java/com/example/replica/composables/YnabScreen.kt
@@ -32,10 +32,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.replica.R
-
 
 @Composable
 fun YnabScreen(backHome: () -> Unit,) {
@@ -242,4 +242,10 @@ fun ElevatedCenterAlignedTopAppBar(backHome: ()-> Unit) {
                     .size(40.dp))
             })
     }
+}
+
+@Preview()
+@Composable
+private fun PreviewLight() {
+    YnabScreen {}
 }
