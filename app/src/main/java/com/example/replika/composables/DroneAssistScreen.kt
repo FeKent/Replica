580d56f80993b1d3a268a63be583730c7cb1f487
SnippyRex <kentf022@gmail.com>
2023-09-11 00:21:46 +0100

    Move DroneAssistScreen composables into Column

--- app/src/main/java/com/example/replika/composables/DroneAssistScreen.kt
+++ app/src/main/java/com/example/replika/composables/DroneAssistScreen.kt
@@ -1,14 +1,17 @@
 package com.example.replika.composables
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 
 @Composable
-fun DroneAssistScreen(backHome:() -> Unit,) {
-
-    Button(onClick = { backHome()}) {
-        Text(text = "Test")
+fun DroneAssistScreen(backHome:() -> Unit) {
+    Column {
+        Button(onClick = { backHome()}) {
+            Text(text = "Test")
+        }
     }
+
 }
