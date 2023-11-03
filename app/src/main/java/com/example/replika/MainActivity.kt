73f698d76bc37725cdd4b9489015c94fb6e23ec6
SnippyRex <kentf022@gmail.com>
2023-11-03 19:34:13 +0000

    Fix FAB positioning
    Use a Scaffold to add the FAB onto the GmailScreen

--- app/src/main/java/com/example/replika/MainActivity.kt
+++ app/src/main/java/com/example/replika/MainActivity.kt
@@ -49,7 +49,7 @@ sealed class Screen(val route: String) {
 fun Replika() {
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = Screen.Gmail.route) {
+    NavHost(navController = navController, startDestination = Screen.Home.route) {
         composable(Screen.Home.route) {
             HomeScreen(
                 ynabScreen = { navController.navigate(Screen.Ynab.route) },
